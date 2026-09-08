67142321959cb04cbc624e5f813107c11fab79bf
Park Junwoo <ays99111@naver.com>
2026-09-08 18:04:45 +0900

    웹앱 Dockerfile을 docker/web/으로 이동, standalone 빌드 설정 추가
    - Dockerfile.web, .dockerignore를 docker/web/ 아래로 정리
    - ignore 파일은 BuildKit 컨벤션에 맞춰 docker/web/Dockerfile.web.dockerignore로 개명
      (컨텍스트는 여전히 레포 루트를 사용)
    - next.config.js에 output: "standalone" 추가해 Dockerfile.web의 런타임 스테이지가
      최소 node_modules만 포함하도록 함

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Docker 이미지를 최소 런타임 파일만 포함하도록 빌드하기 위함 (Dockerfile.web 참고).
+  output: "standalone",
+};
 
 module.exports = nextConfig;
